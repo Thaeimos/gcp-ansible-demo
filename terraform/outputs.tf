c40eebcf7d2fc8b1e268ec288ba41f6524b3434c
Rafa Adorna <rafael.adorna@gmail.com>
2024-02-27 18:52:36 +0100

    Output ssh command.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
 output "ssh_command_connect" {
-    value = "ssh -i ${local.ssh_file_name} ansible@" #${google_compute_instance.ansible.*}"
+  value = toset([
+    for vm in google_compute_instance.ansible :
+    "ssh -i ${local.ssh_file_name} ${split("@", data.google_client_openid_userinfo.me.email)[0]}@${vm.network_interface[0].access_config[0].nat_ip}"
+  ])
 }
