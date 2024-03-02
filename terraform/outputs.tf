8cb5163212985c4ab89fe34ee6be406ee8e6311a
Rafa Adorna <rafael.adorna@gmail.com>
2024-03-02 13:03:21 +0100

    Add outputs.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,20 @@
-# output "ssh_command_connect" {
+output "ssh_command_connect_api" {
+  value = toset([
+    for vm in google_compute_instance.vm_api_instance :
+    "ssh -i ${local.ssh_file_name} ${split("@", data.google_client_openid_userinfo.me.email)[0]}@${vm.network_interface[0].access_config[0].nat_ip}"
+  ])
+}
+
+# output "ssh_command_connect_web" {
 #   value = toset([
-#     for vm in google_compute_instance.ansible :
+#     for vm in google_compute_instance.vm_web_instance :
+#     "ssh -i ${local.ssh_file_name} ${split("@", data.google_client_openid_userinfo.me.email)[0]}@${vm.network_interface[0].access_config[0].nat_ip}"
+#   ])
+# }
+
+# output "ssh_command_connect_db" {
+#   value = toset([
+#     for vm in google_compute_instance.vm_db_instance :
 #     "ssh -i ${local.ssh_file_name} ${split("@", data.google_client_openid_userinfo.me.email)[0]}@${vm.network_interface[0].access_config[0].nat_ip}"
 #   ])
 # }
